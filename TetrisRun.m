57c643757223cee0050be526fd09b5ecef8af05f
tomonori sakagami <sakagami0615@gmail.com>
2020-10-20 06:01:53 +0900

    add:作成途中のメイン関数を追加

---
+++ TetrisRun.m
@@ -0,0 +1,36 @@
+function TetrisRun()
+
+%% パラメータ用意
+% パラメータの読み込み
+global tetrisParam;
+tetrisParam = GetTetrisParameter();
+
+%% テトリミノ用意
+% テトリミノを作成
+tetrisTetrinimoObj = TetrisTetriminos();
+
+%% 初期化処理
+% 盤面を用意
+tetrisBoardObj = TetrisBoard();
+
+% testcode
+blockType = 1;
+rotateCount = 1;
+tetrimino = tetrisTetrinimoObj.GetTetriminoPos(blockType);
+tetrisBoardObj = tetrisBoardObj.PutTetrinino(5, 5, tetrimino{rotateCount}, blockType);
+
+
+%% 画面用意
+
+% メイン画面
+tetrisViewer = TetrisViewer();
+
+tetrisViewer.Draw(tetrisBoardObj.GetBoardDisplayData());
+
+
+%% メインループ
+
+
+%% 終了処理
+
+end
