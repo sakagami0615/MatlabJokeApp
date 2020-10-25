7c4b8083049e023987d7c897d65a7d710aedb993
tomonori sakagami <sakagami0615@gmail.com>
2020-10-25 13:55:54 +0900

    add:パラメータと描画領域を作成

---
+++ minesweeper/MsRun.m
@@ -0,0 +1,20 @@
+function MsRun()
+
+% パラメータの読み込み
+msParam = GetMsParameter();
+
+% Figure作成
+figPosition = [50, 100, msParam.windosSize.width, msParam.windosSize.height];
+fig = figure('Position',figPosition, ...
+             'numbertitle','off', ...
+             'Resize', 'off', ...
+             'Name', msParam.windowTitle);
+
+% Figureに付けるパネルのサイズ計算
+panelPosition = [msParam.boardPanelSize.margin, msParam.boardPanelSize.margin, ...
+                 msParam.boardPanelSize.width, msParam.boardPanelSize.height];
+
+% パネル作成
+panel = uipanel(fig, 'Units','Pixel', 'Position',panelPosition, 'title','');
+
+end
